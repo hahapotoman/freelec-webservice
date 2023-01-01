22dd4ecaabf83094c0608aa1fb77d15f9f5decd8
hahapotoman <hahapotoman@gmail.com>
2023-01-01 23:37:52 +0900

    게시판 수정까지 완료

---
+++ freelec-springboot-webservice/src/main/java/project/service/posts/PostsService.java
@@ -0,0 +1,46 @@
+package project.service.posts;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import project.domain.posts.Posts;
+import project.domain.posts.PostsRepository;
+import project.web.dto.PostsListResponseDto;
+import project.web.dto.PostsResponseDto;
+import project.web.dto.PostsSaveRequestDto;
+import project.web.dto.PostsUpdateRequestDto;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+	private final PostsRepository postsRepository;
+	
+	@Transactional
+	public Long save(PostsSaveRequestDto requestDto) {
+		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+	
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+		
+		posts.update(requestDto.getTitle(), requestDto.getContent());
+		
+		return id;
+	}
+	
+	public PostsResponseDto findById (Long id) {
+		Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+		
+		return new PostsResponseDto(entity);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<PostsListResponseDto> findAllDesc() {
+		return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+	}
+}
